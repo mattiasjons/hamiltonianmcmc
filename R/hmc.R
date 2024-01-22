0dc2760d57e773520f690415d400393ca5a71b15
patte79 <mattias.pj.jonsson@gmail.com>
2024-01-22 15:41:08 +0100

    Fix some file comments/headers...

--- R/hmc.R
+++ R/hmc.R
@@ -1,18 +1,11 @@
-# Hello, world!
+#' Hamiltonian Markov Chain Monte Carlo sampler.
+#'
+#' @description
+#'
+#' `hamiltonian_mcmc()` is the main function of this library, that provides a simple way to run a Hamiltonian MCMC sampler for given density and gradient functions.
+#'
+#' At the moment, no vignette exists.
 #
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
-
 
 #' Leapfrog integrator
 #'
